7fe6bc946d1bd2524279e5cd39048868a110b547
seenso <>
2021-11-29 15:19:14 -0900

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,7 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
   const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -15,15 +16,19 @@ function Form() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    const formData = {
-      firstName: firstName,
-      lastName: lastName
-    };
-    const dataArray = [...submittedData, formData];
-    // props.sendFormDataSomewhere(formData); //normally would send form data to server but no server so removing/not using for now.
-    setSubmittedData(dataArray);
-    setFirstName(""); //clear input field
-    setLastName(""); //clear input field
+    console.log("handleSubmit() has been invoked!");
+
+    if(firstName.length > 0) {
+      const formData = {firstName: firstName, lastName: lastName};
+      const dataArray = [...submittedData, formData];
+      // props.sendFormDataSomewhere(formData); //normally would send form data to server but no server so removing/not using for now.
+      setSubmittedData(dataArray);
+      setFirstName(""); //clear input field
+      setLastName(""); //clear input field
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"]);
+    }
   }
 
   const listOfSubmissions = submittedData.map((data, index) => {
@@ -36,11 +41,19 @@ function Form() {
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleFirstNameChange} value={firstName} />
         <input type="text" onChange={handleLastNameChange} value={lastName} />
-        <button type="submit" onSubmit={handleSubmit}>Submit</button>
+        <button type="submit">Submit</button>
       </form>
+      {/* conditionally render error messages */}
+      {errors.length > 0
+        ? errors.map((error, index) => (
+            <p key={index} style={{ color: "red" }}>
+              {error}
+            </p>
+          ))
+        : null}
       <h3>Submissions</h3>
       {listOfSubmissions}
     </div>
